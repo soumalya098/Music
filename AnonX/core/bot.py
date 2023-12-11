baf346357ddc289491178d0ad5eddcae6b1789ef
ASHWANIashwani <134682768+ASHWANIashwani@users.noreply.github.com>
2023-12-11 12:59:24 +0530

    Update bot.py

--- AnonX/core/bot.py
+++ AnonX/core/bot.py
@@ -12,7 +12,7 @@ class AnonXBot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
-            "™°‌ 🫧 🇴 🇽 𝐘 𝐆 𝐄 𝐍",
+            "˹𝙅𝙚𝙣𝙣𝙞𝙚 ✘ 𝙈𝙪𝙨𝙞𝙘 ˼ ~🎵",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
